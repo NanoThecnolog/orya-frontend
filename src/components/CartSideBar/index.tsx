4380473d2f5929c22d7b1f751cdbb3b2bdac524f
NanoThecnolog <contato@ericssongomes.com>
2025-10-17 19:52:15 -0300

    expande classe ecommerce para gerenciar o cart. Ajusta cartsidebar para usuario controlar a quantidade de produtos dentro do carrinho

--- src/components/CartSideBar/index.tsx
+++ src/components/CartSideBar/index.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { useMain } from "@/contexts/mainContext";
 import { format } from "@/utils/formatContent";
+import Image from "next/image";
+import { FaTrash } from "react-icons/fa";
+import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import { Ecommerce } from "@/services/classes/ecommerce";
+import { Product } from "@/@types/nuvemshop/products";
 
 type CartItem = {
     id: number;
@@ -15,19 +20,28 @@ export default function CartSidebar() {
     const { cartOpen, setCartOpen } = useMain()
     const { cartItems, setCartItems } = useMain()
     const [totalPrice, setTotalPrice] = useState<number>(0)
+    const [subTotal, setSubTotal] = useState<number>(0)
+    const [shipping, setShipping] = useState<number>(0)
 
     const toggleCart = () => setCartOpen(!cartOpen);
 
     useEffect(() => {
+        setTotalPrice(cartItems.reduce((sum, item) => sum + Number(item.product?.variants?.[0]?.price) * item.amount, 0) + shipping)
+        setSubTotal(cartItems.reduce((sum, item) => sum + Number(item.product?.variants?.[0]?.price) * item.amount, 0))
+    }, [cartItems, shipping])
 
-        setTotalPrice(cartItems.reduce((sum, item) => sum + Number(item.product?.variants?.[0]?.price) * item.amount, 0))
-    }, [cartItems])
+    const handleAmount = (product: Product, operation: "add" | "sub" | "remove") => {
+        const ecommerce = new Ecommerce(cartItems, setCartItems)
+        if (operation === "add") ecommerce.addToCart(product)
+        if (operation === "sub") ecommerce.subFromCart(product)
+        else ecommerce.deleteProductFromCart(product)
+    }
 
     return (
         <>
             <aside className={`${styles.cartSidebar} ${cartOpen ? styles.open : ""}`}>
                 <div className={styles.header}>
-                    <span>Carrinho</span>
+                    <h3>Carrinho</h3>
                     <button onClick={toggleCart}>&times;</button>
                 </div>
 
@@ -35,12 +49,38 @@ export default function CartSidebar() {
                     {cartItems.length === 0 && <p>Carrinho vazio</p>}
                     {cartItems.map(item => (
                         <div key={item.product.id} className={styles.item}>
-                            <span className="name">{item.product.name.pt}</span>
-                            <span>{item.amount}</span>
-                            <span className="price">{format.price(Number(item.product.variants?.[0]?.price).toFixed(2))}</span>
+                            <div className={styles.imageContainer}>
+                                <Image
+                                    src={item.product.images[1].src}
+                                    alt={item.product.name.pt}
+                                    fill
+                                    priority={false}
+                                    className={styles.image}
+                                />
+                            </div>
+
+                            <div className={styles.cartItemDetails}>
+                                <h4 className={styles.name}>{item.product.name.pt}</h4>
+                                <div className={styles.amount}>
+                                    <IoIosRemove className={styles.removeButton} size={25} onClick={() => handleAmount(item.product, "sub")} />
+                                    <p>{item.amount}</p>
+                                    <IoIosAdd className={styles.addButton} size={25} onClick={() => handleAmount(item.product, "add")} />
+                                </div>
+                            </div>
+                            <div className={styles.priceContainer}>
+                                <p className={styles.price}>{format.price(item.product.variants?.[0]?.price)}</p>
+                                <FaTrash title="excluir" onClick={() => handleAmount(item.product, "remove")} />
+                            </div>
                         </div>
                     ))}
+                    {
+                        cartItems.length > 0 && <div className={styles.subTotalContainer}>
+                            <h4>subtotal (sem frete)</h4>
+                            <p>{format.price(subTotal.toString())}</p>
+                        </div>
+                    }
                 </div>
+
                 <div>
                     <button onClick={() => setCartItems([])}>Limpar carrinho</button>
                 </div>
--- src/services/classes/ecommerce.ts
+++ src/services/classes/ecommerce.ts
@@ -7,23 +7,42 @@ export class Ecommerce {
     private setCartItems: Dispatch<SetStateAction<CartProps[]>>;
 
     constructor(cartItems: CartProps[], setCartItems: Dispatch<SetStateAction<CartProps[]>>) {
-        this.cartItems = cartItems,
-            this.setCartItems = setCartItems
+        this.cartItems = cartItems;
+        this.setCartItems = setCartItems;
+    }
+    private hasItem(product: Product): boolean {
+        const hasItem = this.cartItems.find(item => item.product.id === product.id)
+        if (hasItem) return true
+        return false
+    }
+    private updateCart(updater: SetStateAction<CartProps[]>): void {
+        this.setCartItems(updater)
+    }
+    private changeAmount(productID: number, delta: number): void {
+        this.updateCart(prev =>
+            prev.map(item =>
+                item.product.id === productID
+                    ? { ...item, amount: item.amount + delta }
+                    : item
+            ).filter(item => item.amount > 0)
+        )
     }
 
-    addToCart(product: Product) {
-        const hasItem = this.cartItems.find(item => item.product.id === product.id)
-        if (hasItem) {
-            this.setCartItems((prev) =>
-                prev.map(item =>
-                    item.product.id === product.id
-                        ? { ...item, amount: item.amount + 1 }
-                        : item
-                )
-            );
-        } else {
-            this.setCartItems((prev) => [...prev, { product, amount: 1 }]);
-        }
+    addToCart(product: Product): void {
+        const hasItem = this.hasItem(product)
+        if (hasItem) this.changeAmount(product.id, 1)
+        else this.setCartItems((prev) => [...prev, { product, amount: 1 }]);
+    }
+    subFromCart(product: Product): void {
+        const hasItem = this.hasItem(product)
+        if (hasItem) this.changeAmount(product.id, -1)
+    }
+    deleteProductFromCart(product: Product): void {
+        const hasItem = this.hasItem(product)
+        if (hasItem) this.updateCart(prev => prev.filter(item => item.product.id !== product.id))
+    }
+    clearCart(): void {
+        this.setCartItems([])
     }
 }
 
